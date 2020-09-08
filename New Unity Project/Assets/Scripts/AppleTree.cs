7ead21417c8e95c474c87c97d7def82db5b08a18
nerad25 <dmcrull@yahoo.com>
2020-09-08 16:53:17 -0500

    Added up to coding applePicker
    tutorial up to pg 471, however i cant add script to prefab

---
+++ New Unity Project/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    //[Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which apple tree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 10f;
+
+    // Chance that the AppleTree will change directrions
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apples will be instaitiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
