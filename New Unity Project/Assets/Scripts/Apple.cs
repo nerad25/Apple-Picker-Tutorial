64d425aafaff3898915398a037c674f65bfa0581
Hayley Edwards <hayedwa@siue.edu>
2020-09-13 21:08:33 -0700

    Added (Most Of) Basic Scoring Functionality
    Added a score counter, points for each caught apple, and basket loss when an apple is missed. If you run out of baskets, the game does not yet end--a check for lack of baskets must be implemented (which will then trigger the Game Over screen).

--- New Unity Project/Assets/Scripts/Apple.cs
+++ New Unity Project/Assets/Scripts/Apple.cs
@@ -18,6 +18,11 @@ public class Apple : MonoBehaviour
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+
+            apScript.AppleDestroyed();
         }
     }
 }
--- New Unity Project/Assets/Scripts/Basket.cs
+++ New Unity Project/Assets/Scripts/Basket.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -30,6 +38,13 @@ public class Basket : MonoBehaviour
         if ( collidedWith.tag == "Apple" )
         {
             Destroy( collidedWith );
+
+
+            int score = int.Parse( scoreGT.text );
+
+            score += 100;
+
+            scoreGT.text = score.ToString();
         }
     }
 }
